da692c1fea9d61f306ad91f1827c1d1d6153e0e9
TonyPham-Dev <phamvancong300701@gmail.com>
2024-08-13 10:52:11 +0700

    feat skill

--- Source/Web/src/pages/host/components/skill/Skill.js
+++ Source/Web/src/pages/host/components/skill/Skill.js
@@ -7,20 +7,24 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import './skill.css';
+import constants from '../../../../constants';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    backgroundColor: "#929291",
+    backgroundColor: constants.backgroundColorHead,
     color: theme.palette.common.white,
-    border: "1px solid #fff",
+    border: '1px solid #fff',
   },
   body: {
     fontSize: 14,
-    border: "1px solid #E0E0E0", 
-    padding: "10px 16px",
-    backgroundColor: "#fff",
+    height: constants.heightTableRow,
+    border: '1px solid #E0E0E0',
+    padding: '0px 16px',
+    backgroundColor: '#fff',
   },
 }))(TableCell);
+
 const StyledTableRow = withStyles((theme) => ({
   root: {
     '&:nth-of-type(odd)': {
@@ -29,19 +33,7 @@ const StyledTableRow = withStyles((theme) => ({
   },
 }))(TableRow);
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-  lastRow: {
-    '& td': {
-      borderBottom: '2px solid #636363', 
-    },
-  },
-  categoryCell: {
-    borderBottom: '2px solid #636363',
-  },
-});
+const useStyles = makeStyles(constants.tableRowStyles);
 
 function createData(data) {
   return data;
@@ -55,6 +47,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 1',
@@ -62,6 +55,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
 
     {
@@ -70,6 +64,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
   ]),
   createData([
@@ -79,6 +74,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 2',
@@ -86,6 +82,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 2',
@@ -93,6 +90,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 2',
@@ -100,7 +98,8 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
-    }
+      metric5: 1000,
+    },
   ]),
   createData([
     {
@@ -109,13 +108,14 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 3',
       metric1: 200,
       metric2: 100,
       metric3: 70.9,
-    }
+    },
   ]),
   createData([
     {
@@ -124,6 +124,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 4',
@@ -131,6 +132,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 4',
@@ -138,6 +140,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 4',
@@ -145,6 +148,7 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
     {
       name: 'Item 4',
@@ -152,45 +156,86 @@ const rows = [
       metric2: 100,
       metric3: 70.9,
       metric4: 1000,
+      metric5: 1000,
     },
-  
-  ])
+  ]),
 ];
 
 export default function CustomTable() {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper} className={classes.container}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Category</StyledTableCell>
-            <StyledTableCell align="center">Metric 1</StyledTableCell>
-            <StyledTableCell align="center">Metric 2</StyledTableCell>
-            <StyledTableCell align="center">Metric 3</StyledTableCell>
-            <StyledTableCell align="center">Metric 4</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((group) => (
-            group.map((data, dataIndex) => {
-              const isLastRow = dataIndex === group.length - 1;
-              return (
-                <StyledTableRow key={data.name} className={isLastRow ? classes.lastRow : ''}>
-                  <StyledTableCell component="th" scope="row" className={isLastRow ? classes.categoryCell : ''}>
-                    {data.name}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">{data.metric1}</StyledTableCell>
-                  <StyledTableCell align="center">{data.metric2}</StyledTableCell>
-                  <StyledTableCell align="center">{data.metric3}</StyledTableCell>
-                  <StyledTableCell align="center">{data.metric4}</StyledTableCell>
-                </StyledTableRow>
-              );
-            })
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div className="container">
+      <TableContainer component={Paper} className={classes.container}>
+        <div className="container-header">
+          <div className="performance-text">スキル別パフォーマンス</div>
+          <button className="button-csv">CSV 出力</button>
+        </div>
+        {/* <div>
+          <div>期間指定</div>
+          <div></div>
+        </div> */}
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead >
+            <TableRow >
+              <StyledTableCell>項目</StyledTableCell>
+              <StyledTableCell align="center">
+                <div>
+                  <span className="languages">EN</span>
+                  <span>英語</span>
+                </div>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <div>
+                  <span className="languages">CN</span>
+                  <span>中国語</span>
+                </div>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <div>
+                  <span className="languages">KR</span>
+                  <span>韓国語</span>
+                </div>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <div>
+                  <span className="languages">ES</span>
+                  <span>スペイン語</span>
+                </div>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <div>
+                  <span className="languages">PT</span>
+                  <span>ポルトガル語</span>
+                </div>
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((group) =>
+              group.map((data, dataIndex) => {
+                const isLastRow = dataIndex === group.length - 1;
+                return (
+                  <StyledTableRow key={data.name} className={isLastRow ? classes.lastRow : ''}>
+                    <StyledTableCell
+                      component="th"
+                      scope="row"
+                      className={isLastRow ? classes.categoryCell : ''}
+                    >
+                      {data.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">{data.metric1}</StyledTableCell>
+                    <StyledTableCell align="center">{data.metric2}</StyledTableCell>
+                    <StyledTableCell align="center">{data.metric3}</StyledTableCell>
+                    <StyledTableCell align="center">{data.metric4}</StyledTableCell>
+                    <StyledTableCell align="center">{data.metric5}</StyledTableCell>
+                  </StyledTableRow>
+                );
+              })
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
