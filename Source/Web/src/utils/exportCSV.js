2543eceaf2025ca15eba38765f3973ac6ed2f081
TonyPham-Dev <phamvancong300701@gmail.com>
2024-08-15 15:36:13 +0700

    fix export csv

--- Source/Web/src/utils/exportCSV.js
+++ Source/Web/src/utils/exportCSV.js
@@ -8,16 +8,15 @@ import { getYearMonthDay } from './formatDate';
  * @param {Date} dateMonthDay
  */
 export const exportToCSV = (rows, fileName = 'ExportedData.csv', dateYearMonth, dateMonthDay) => {
-  const headers = ['項目',
+  const headers = [
+    '項目',
     `年 / 月: ${dateYearMonth.getFullYear()}-${String(dateYearMonth.getMonth() + 1).padStart(2, '0')}`,
-    `月 / 日: ${String(dateMonthDay.getMonth() + 1).padStart(2, '0')}-${String(dateMonthDay.getDate()).padStart(2, '0')}`
+    `月 / 日: ${String(dateMonthDay.getMonth() + 1).padStart(2, '0')}-${String(dateMonthDay.getDate()).padStart(2, '0')}`,
   ];
 
-  const csvContent = rows.map(group =>
-    group.map(data =>
-      `${data.name},${data.metric1},${data.metric2}`
-    ).join('\n')
-  ).join('\n');
+  const csvContent = rows
+    .map((group) => group.map((data) => `${data.name},${data.metric1},${data.metric2}`).join('\n'))
+    .join('\n');
 
   const csvData = [headers.join(','), csvContent].join('\n');
   const bom = '\uFEFF';
@@ -25,17 +24,27 @@ export const exportToCSV = (rows, fileName = 'ExportedData.csv', dateYearMonth, 
   saveAs(blob, fileName);
 };
 
-export const exportToCSVSkill = (rows, fileName = 'ExportedData.csv', fromDayMonthYear, toDayMonthYear) => {
-  const headers = ['項目',
-    `年 / 月: ${getYearMonthDay(fromDayMonthYear)}`,
-    `月 / 日: ${getYearMonthDay(toDayMonthYear)}`
+export const exportToCSVSkill = (
+  rows,
+  fileName = 'ExportedData.csv',
+  fromDayMonthYear,
+  toDayMonthYear
+) => {
+  const headers = [
+    '項目, EN英語, CN中国語, KR韓国語, ESスペイン語, PTポルトガル語',
+    `年 / 月 / 日: ${getYearMonthDay(fromDayMonthYear)}`,
+    `年 / 月 / 日: ${getYearMonthDay(toDayMonthYear)}`,
   ];
 
-  const csvContent = rows.map(group =>
-    group.map(data =>
-      `${data.name},${data.metric1},${data.metric2},${data.metric3},${data.metric4},${data.metric5}`
-    ).join('\n')
-  ).join('\n');
+  const csvContent = rows
+    .map((group) => {
+      return group
+        .map((data) => {
+          return `${data.name},${data.metric1},${data.metric2},${data.metric3},${data.metric4},${data.metric5}`;
+        })
+        .join('\n');
+    })
+    .join('\n');
 
   const csvData = [headers.join(','), csvContent].join('\n');
   const bom = '\uFEFF';
