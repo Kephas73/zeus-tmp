aeaaead6a1b216f57fe088e7825774e6e05d01fd
ducbinhbld <ducbinhbld@gmail.com>
2024-08-26 17:21:53 +0700

    fix customer datepicker

--- Source/Web/src/pages/host/components/header/Header.js
+++ Source/Web/src/pages/host/components/header/Header.js
@@ -1,9 +1,18 @@
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
-import './header.css';
 
 const useStyles = makeStyles(() => ({
+  hostHeader: {
+    padding: '0px 15px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  hostHeaderIcon: {
+    display: 'flex',
+    alignItems: 'center',
+  },
   customIcon: {
     color: 'var(--text-color-gray)',
     fontSize: '3rem',
@@ -12,20 +21,29 @@ const useStyles = makeStyles(() => ({
     fontWeight: 'bold',
     color: 'var(--text-color-gray)',
   },
+  hostCloseButtonElement: {
+    padding: '2px 33px',
+    backgroundColor: 'var(--background-color-btn-close)',
+    border: 'none',
+    borderRadius: '5px',
+    color: 'var(--color-white)',
+    cursor: 'pointer',
+    fontSize: '12px',
+  },
 }));
 
 const Header = ({ setOpen }) => {
   const classes = useStyles();
   return (
     <>
-      <div className="host-header">
-        <div className="host-header-icon">
+      <div className={classes.hostHeader}>
+        <div className={classes.hostHeaderIcon}>
           <span>
             <PersonIcon className={classes.customIcon} />
           </span>
           <span className={classes.hostCloseButton}>センターパフォーマンス</span>
         </div>
-        <button onClick={() => setOpen(false)} className="host-close-button">
+        <button onClick={() => setOpen(false)} className={classes.hostCloseButtonElement}>
           閉じる
         </button>
       </div>
