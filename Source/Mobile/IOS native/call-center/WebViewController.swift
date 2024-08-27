6037802f8594c88cd43a1ed50134ec094d472c12
nssoldier <hoangbv.nde17048@vtc.edu.vn>
2024-08-27 17:34:42 +0700

    add new url and console log

--- Source/Mobile/IOS native/call-center/WebViewController.swift
+++ Source/Mobile/IOS native/call-center/WebViewController.swift
@@ -1,8 +1,9 @@
 import SwiftUI
 import WebKit
+import UIKit
 
 // Định nghĩa WebViewController
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKScriptMessageHandler, WKUIDelegate {
     var webView: WKWebView!
 
     override func viewDidLoad() {
@@ -11,6 +12,20 @@ class WebViewController: UIViewController {
         let webConfiguration = WKWebViewConfiguration()
         let userContentController = WKUserContentController()
         let scriptSource = """
+                (function() {
+                    window.webkit.messageHandlers.consoleLog.postMessage({type: 'test', message: 'console log connected!'});
+                    function interceptConsole(method) {
+                        var original = console[method];
+                        console[method] = function() {
+                            var message = Array.prototype.slice.call(arguments).join(' ');
+                            if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.consoleLog) {
+                                window.webkit.messageHandlers.consoleLog.postMessage({type: method, message: message});
+                            }
+                            original.apply(console, arguments);
+                        };
+                    }
+                    ['log', 'warn', 'error'].forEach(interceptConsole);
+                })();
         document.addEventListener('webkitfullscreenchange', function() {
             if (document.webkitIsFullScreen) {
                 document.webkitExitFullscreen();
@@ -18,23 +33,36 @@ class WebViewController: UIViewController {
         });
         """
         let script = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        userContentController.add(self, name: "consoleLog")
         userContentController.addUserScript(script)
-        webConfiguration.userContentController = userContentController
         webConfiguration.allowsInlineMediaPlayback = true
         webConfiguration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        webConfiguration.userContentController = userContentController
 
         webView = WKWebView(frame: self.view.bounds, configuration: webConfiguration)
         webView.uiDelegate = self
         self.view.addSubview(webView)
 
-        if let url = URL(string: "https://example.likeness-online.com") {
+        if let url = URL(string: "https://example.videoschat.net") {
             let request = URLRequest(url: url)
             webView.load(request)
+            webView.evaluateJavaScript("document.readyState", completionHandler: { (result, error) in
+                if let readyState = result as? String, readyState == "complete" {
+                    print("Dom loaded!")
+                }
+            })
+        }
+    }
+
+    // Xử lý các thông điệp từ JavaScript
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if let messageBody = message.body as? [String: String],
+           let type = messageBody["type"],
+           let logMessage = messageBody["message"] {
+            print("Console \(type): \(logMessage)")
         }
     }
 }
-
-extension WebViewController: WKUIDelegate {}
 
 // Tạo UIViewControllerRepresentable Wrapper
 struct WebViewControllerRepresentable: UIViewControllerRepresentable {
